926de28e00727111893e3799109d9fd8c61269db
hmg29 <hmg29@duke.edu>
2024-04-23 09:04:25 -0400

    working through api

---
+++ app.mjs
@@ -0,0 +1,10 @@
+/*
+
+The assignment is free-form: create a client side web application that makes use of two or more 3rd party APIs.
+
+Turn in your code as a repository to GradeScope. Include a README explaining what the application is and which 3rd party APIs were used.
+
+Completing this assignment will be worth an additional 2% added to your course grade. In other words, if you would have gotten a B- with 82% of the points for the course and completed this assignment, you will then be given a course grade that corresponds to 84%, a B, instead. 
+
+
+*/
